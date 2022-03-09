cf27a3c1c32f8f857a0f7dac10018cbed4e4f748
krijk <kenvanrijk@gmail.com>
2022-03-09 10:36:34 +0900

    - clean up

--- ex01/lib/main.dart
+++ ex01/lib/main.dart
@@ -31,13 +31,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -51,23 +44,19 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
+          children: const <Widget>[
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+              'Drop files the area.'
+                  '\n(Only English file names)'
+                  '\nFile pathes:',
             ),
-            const ExmapleDragTarget(),
+            SizedBox(
+              height: 20,
+            ),
+            ExmapleDragTarget(),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
